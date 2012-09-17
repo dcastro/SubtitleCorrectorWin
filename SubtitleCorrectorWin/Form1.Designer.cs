d57da65331fda7a0f0d020eb9e1fe7565a4f337f
Diogo Castro <diogo.filipe.acastro@gmail.com>
2012-09-17 23:40:50 +0100

    added controls for seconds/milliseconds

--- SubtitleCorrectorWin/Form1.Designer.cs
+++ SubtitleCorrectorWin/Form1.Designer.cs
@@ -31,6 +31,12 @@
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.selectFileButton = new System.Windows.Forms.Button();
             this.filenameLabel = new System.Windows.Forms.Label();
+            this.secondsInput = new System.Windows.Forms.NumericUpDown();
+            this.millisecondsInput = new System.Windows.Forms.NumericUpDown();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.millisecondsLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.secondsInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.millisecondsInput)).BeginInit();
             this.SuspendLayout();
             // 
             // openFileDialog
@@ -57,16 +63,54 @@
             this.filenameLabel.TabIndex = 1;
             this.filenameLabel.Text = "Please select a file";
             // 
+            // secondsInput
+            // 
+            this.secondsInput.Location = new System.Drawing.Point(118, 139);
+            this.secondsInput.Name = "secondsInput";
+            this.secondsInput.Size = new System.Drawing.Size(82, 20);
+            this.secondsInput.TabIndex = 2;
+            // 
+            // millisecondsInput
+            // 
+            this.millisecondsInput.Location = new System.Drawing.Point(240, 139);
+            this.millisecondsInput.Name = "millisecondsInput";
+            this.millisecondsInput.Size = new System.Drawing.Size(82, 20);
+            this.millisecondsInput.TabIndex = 3;
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Location = new System.Drawing.Point(115, 108);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(49, 13);
+            this.secondsLabel.TabIndex = 4;
+            this.secondsLabel.Text = "Seconds";
+            // 
+            // millisecondsLabel
+            // 
+            this.millisecondsLabel.AutoSize = true;
+            this.millisecondsLabel.Location = new System.Drawing.Point(237, 108);
+            this.millisecondsLabel.Name = "millisecondsLabel";
+            this.millisecondsLabel.Size = new System.Drawing.Size(64, 13);
+            this.millisecondsLabel.TabIndex = 5;
+            this.millisecondsLabel.Text = "Milliseconds";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(476, 282);
+            this.Controls.Add(this.millisecondsLabel);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.millisecondsInput);
+            this.Controls.Add(this.secondsInput);
             this.Controls.Add(this.filenameLabel);
             this.Controls.Add(this.selectFileButton);
             this.Name = "Form1";
             this.Text = "Subtitle Corrector";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.secondsInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.millisecondsInput)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +121,10 @@
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.Button selectFileButton;
         private System.Windows.Forms.Label filenameLabel;
+        private System.Windows.Forms.NumericUpDown secondsInput;
+        private System.Windows.Forms.NumericUpDown millisecondsInput;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label millisecondsLabel;
     }
 }
 
